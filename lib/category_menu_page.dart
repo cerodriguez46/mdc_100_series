9a150559f6fb2e02e7bdf3dfffd11a4d5fae6d4a
Chris Rodriguez <cerdevelopers@gmail.com>
2019-03-28 21:03:05 -0500

    UPDATE: • Menu added to the backdrop

--- lib/category_menu_page.dart
+++ lib/category_menu_page.dart
@@ -18,36 +18,37 @@ class CategoryMenuPage extends StatelessWidget {
 
   Widget _buildCategory(Category category, BuildContext context) {
     final categoryString =
-        category.toString().replaceAll('Category.', '').toUpperCase();
+    category.toString().replaceAll('Category.', '').toUpperCase();
     final ThemeData theme = Theme.of(context);
     return GestureDetector(
       onTap: () => onCategoryTap(category),
       child: category == currentCategory
           ? Column(
-              children: <Widget>[
-                SizedBox(height: 16.0),
-                Text(
-                  categoryString,
-                  style: theme.textTheme.body2,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 14.0),
-                Container(
-                  width: 70.0,
-                  height: 2.0,
-                  color: kShrinePink400,
-                ),
-              ],
-            )
+        children: <Widget>[
+          SizedBox(height: 16.0),
+          Text(
+            categoryString,
+            style: theme.textTheme.body2,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 14.0),
+          Container(
+            width: 70.0,
+            height: 2.0,
+            color: kShrinePink400,
+          ),
+        ],
+      )
           : Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Text(
-                categoryString,
-                style: theme.textTheme.body2
-                    .copyWith(color: kShrineBrown900.withAlpha(153)),
-                textAlign: TextAlign.center,
-              ),
-            ),
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: Text(
+          categoryString,
+          style: theme.textTheme.body2.copyWith(
+              color: kShrineBrown900.withAlpha(153)
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 
